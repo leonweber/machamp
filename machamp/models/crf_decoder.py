Fix loss scaling + add print to keep track of dataset mixing during in accumulated batches

machamp/models/crf_decoder.py
@@ -195,7 +195,7 @@ class MachampCrfTagger(Model):
             log_likelihood = self.crf(logits, gold_labels, crf_mask)
 
             output["loss"] = -log_likelihood * self.loss_weight
-            output['loss'] /= torch.log(logits.shape[-1])
+            output['loss'] /= torch.log(torch.tensor(logits.shape[-1]))
 
             # Represent viterbi tags as "class probabilities" that we can
             # feed into the metrics

machamp/models/sentence_decoder.py
@@ -101,7 +101,7 @@ class MachampClassifier(Model):
 
         if gold_labels != None:
             output_dict['loss'] = self._loss(logits, gold_labels.long().view(-1)) * self.loss_weight
-            output_dict['loss'] /= torch.log(logits.shape[-1])
+            output_dict['loss'] /= torch.log(torch.tensor(logits.shape[-1]))
             for metric in self.metrics.values():
                 metric(logits, gold_labels)
 

machamp/models/seq2seq_decoder.py
@@ -355,7 +355,7 @@ class MachampSeq2SeqDecoder(Model):
             target_mask = util.get_text_field_mask(target_tokens)
             loss = self._get_loss(logits, targets, target_mask) * self.loss_weight
             output_dict["loss"] = loss
-            output_dict['loss'] /= torch.log(logits.shape[-1])
+            output_dict['loss'] /= torch.log(torch.tensor(logits.shape[-1]))
 
         return output_dict
 

machamp/models/tag_decoder.py
@@ -137,7 +137,7 @@ class MachampTagger(Model):
             else:
                 tag_mask = mask
             output_dict["loss"] = sequence_cross_entropy_with_logits(logits, gold_labels, tag_mask) * self.loss_weight
-            output_dict['loss'] /= torch.log(logits.shape[-1])
+            output_dict['loss'] /= torch.log(torch.tensor(logits.shape[-1]))
             for metric in self.metrics.values():
                 metric(class_probabilities, gold_labels, mask)
         return output_dict

machamp/modules/bucket_batch_sampler.py
@@ -5,7 +5,7 @@ import random
 from typing import List, Iterable, Tuple, Iterator, TypeVar
 from itertools import islice, zip_longest
 
-
+import numpy as np
 from allennlp.common.checks import ConfigurationError
 #from allennlp.common.util import lazy_groups_of
 from allennlp.data.instance import Instance
@@ -204,6 +204,7 @@ class BucketBatchSampler(BatchSampler):
 
             # collect all batches
             this_epoch_all_batches = []
+
             for dataset_idx in range(len(all_batches)):
                 new_size = new_sizes[dataset_idx]
                 #random.shuffle(all_batches[dataset_idx])
@@ -212,7 +213,22 @@ class BucketBatchSampler(BatchSampler):
                 this_epoch_all_batches += all_batches[dataset_idx][:new_size]
 
             # shuffle all batches
-            random.shuffle(this_epoch_all_batches)
+            this_epoch_dataset_indexes = []
+            for i, size in enumerate(new_sizes):
+                this_epoch_dataset_indexes.extend([i] * size)
+            this_epoch_dataset_indexes = np.array(this_epoch_dataset_indexes)
+            this_epoch_all_batches = np.array(this_epoch_all_batches, dtype=object)
+            indices = np.arange(len(this_epoch_all_batches))
+            np.random.shuffle(indices)
+            this_epoch_all_batches = this_epoch_all_batches[indices]
+            this_epoch_dataset_indexes = this_epoch_dataset_indexes[indices]
+
+            window_size = 4 * 8 # num GPUs * num gradient accumulation
+            mixing_values = []
+            for i in range(len(this_epoch_dataset_indexes) // window_size):
+                mixing_values.append(len(np.unique(this_epoch_dataset_indexes[i * window_size: (i+1) * window_size])) / window_size)
+            print(f"Avg. mixing value: {np.mean(mixing_values)}")
+
             for batch in this_epoch_all_batches:
                 if len(batch) > 0:
                     yield batch